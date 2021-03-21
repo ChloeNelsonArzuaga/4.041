06f1e7e928da652f99f07369139a60cd901d087b
ChloeNelsonArzuaga <chloenelsonarzuaga@gmail.com>
2021-03-20 23:46:42 -0400

    frame od research done

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 let chocolateButton = document.getElementById('chocolate');
+let flavorButton = document.getElementById('flavor');
+let extruderButton = document.getElementById('extruder');
+let barButton = document.getElementById('bar');
 
 chocolateButton.onclick = () => {
     window.location.href='Chocolate/index.html'
+}
+flavorButton.onclick = () => {
+  window.location.href='Flavor/index.html'
 }
